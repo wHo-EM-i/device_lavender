380b5395eddaa152fe6901a9b196a3892fd067e5
ImSpiDy <SpiDy2713@gmail.com>
2023-07-25 20:23:35 +0530

    lavender: RemovePackages: update list
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,13 +4,66 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES :=  \
-    Maps Youtube TurboPrebuilt CalendarGoogle \
-    RecorderPrebuilt PrebuiltGmail Gmail2 \
-    Drive AndroidAutoStubPrebuilt AndroidAutoStub \
-    Wellbeing WellbeingPrebuilt PersonalSafety \
-    TurboAdapter SafetyHubPrebuilt arcore TagGoogle \
-    CalendarGooglePrebuilt Photos
+LOCAL_OVERRIDES_PACKAGES := \
+	AmbientSensePrebuilt \
+	AppDirectedSMSService \
+	arcore \
+	CarrierSetup \
+	Chrome \
+	Chrome-Stub \
+	ConnMO \
+	ConnMetrics \
+	DCMO \
+	DevicePolicyPrebuilt \
+	DMService \
+	Drive \
+	Via \
+	GCS \
+	GoogleCamera \
+	MaestroPrebuilt \
+	Maps \
+	MyVerizonServices \
+	PixelLiveWallpaperPrebuilt \
+	Gmail \
+	Gmail2 \
+	PrebuiltGmail \
+	Recorder \
+	RecorderPrebuilt \
+	Gallery \
+	Gallery2 \
+	StagWalls \
+	PulseMusic \
+	SafetyHubPrebuilt \
+	SCONE \
+	ScribePrebuilt \
+	Showcase \
+	SoundAmplifierPrebuilt \
+	SprintDM \
+	SprintHM \
+	Tycho \
+	FM2 \
+	WellbeingPrebuilt \
+	Calendar \
+	CalendarGoogle \
+	CalendarGooglePrebuilt \
+	USCCDM \
+	AndroidAutoStub \
+	AndroidAutoStubPrebuilt \
+	Videos \
+	Video \
+	VZWAPNLib \
+	VzwOmaTrigger \
+	WallpapersBReel2020 \
+	YouTube \
+	YouTubeMusicPrebuilt \
+	TurboPrebuilt \
+	TagGoogle \
+	talkback \
+	Updater \
+	Symphonica \
+	Gallery2 \
+	Music \
+	PersonalSafety
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
