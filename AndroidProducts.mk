bf29e1f5964910521e1f528983592fcadad08c27
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-07-27 04:31:49 +0530

    Revert "lavender: Awaken bringup"
    This reverts commit c44087cde471a53e92bffbd33dae74197f787f7e.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/awaken_lavender.mk
+    $(LOCAL_DIR)/arrow_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    awaken_lavender-user \
-    awaken_lavender-userdebug \
-    awaken_lavender-eng
+    arrow_lavender-user \
+    arrow_lavender-userdebug \
+    arrow_lavender-eng
