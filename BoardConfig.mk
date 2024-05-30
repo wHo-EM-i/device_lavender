cdb8503d7b25423d65ead8f07f3917b0eb8bae0f
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-05-30 17:28:01 +0530

    lavender: Initial bring-up for combined tree
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,8 +31,15 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/c0c4000.sdhci
-TARGET_KERNEL_CONFIG := lavender_defconfig
 BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_KERNEL_VERSION ?= 4.19
+
+ifeq ($(TARGET_KERNEL_VERSION),4.19)
+TARGET_KERNEL_CONFIG := lavender_defconfig
+else ifeq ($(TARGET_KERNEL_VERSION),4.4)
+TARGET_KERNEL_CONFIG := lavender-perf_defconfig
+endif
+
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 
+# files that live under device/qcom/common/rootdir/etc/
+
+ifeq ($(TARGET_KERNEL_VERSION),4.19)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
@@ -17,6 +20,26 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
+else ifeq ($(TARGET_KERNEL_VERSION),4.4)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab_k4_4.qcom
+LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab_k4_4.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.device.rc
